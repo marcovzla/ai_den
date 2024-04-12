c0b4190fc2ee457f65f4531447e97af8080f26bd
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2024-04-12 10:18:54 -0700

    dataclasses utils

--- src/ai_den/utils/dataclasses.py
+++ src/ai_den/utils/dataclasses.py
@@ -18,42 +18,63 @@ def is_dataclass_instance(obj: Any) -> bool:
 
 
 @overload
-def from_data(data_class: type[T], data: Mapping[str, Any]) -> Optional[T]:
+def from_data(
+        data_class: type[T],
+        data: Mapping[str, Any],
+        *,
+        check_types: bool = False,
+) -> Optional[T]:
     ...
 
 
 @overload
-def from_data(data_class: type[T], data: Sequence[Mapping[str, Any]]) -> list[T]:
+def from_data(
+        data_class: type[T],
+        data: Sequence[Mapping[str, Any]],
+        *,
+        check_types: bool = False,
+) -> list[T]:
     ...
 
 
-def from_data(data_class, data):
+def from_data(data_class, data, *, check_types: bool = False):
     if isinstance(data, Sequence):
-        results = []
-        for d in data:
-            if obj := from_data(data_class, d):
-                results.append(obj)
-        return results
+        return [
+            obj
+            for datum in data
+            if (obj := from_data(data_class, datum))
+        ]
 
     if data:
-        return dacite.from_dict(data_class, data)
+        return dacite.from_dict(data_class, data, dacite.Config(check_types=check_types))
 
 
-def from_json(data_class: type[T], s: str) -> T | list[T]:
-    return from_data(data_class, json.loads(s))
+def from_json(
+        data_class: type[T],
+        s: str,
+        *,
+        check_types: bool = False,
+) -> T | list[T]:
+    return from_data(data_class, json.loads(s), check_types=check_types)
 
 
 def to_json(
         obj: Any,
         *,
         ensure_ascii: bool = False,
-        indent: Optional[int] = None,
+        allow_nan: bool = True,
+        indent: Optional[int | str] = None,
         separators: Optional[tuple[str, str]] = None,
         sort_keys: bool = False,
 ) -> str:
+    if isinstance(obj, Sequence):
+        obj = [dataclasses.asdict(x) for x in obj]
+    else:
+        obj = dataclasses.asdict(obj)
     return json.dumps(
-        dataclasses.asdict(obj),
+        obj,
         ensure_ascii=ensure_ascii,
+        allow_nan=allow_nan,
         indent=indent,
         separators=separators,
         sort_keys=sort_keys,
